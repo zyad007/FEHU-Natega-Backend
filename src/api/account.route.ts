df51d264c09ff91fc9fbcc5628c22c3b22a0daf1
Zyad Abdul-Nasser <zizo_007@live.com>
2024-01-22 22:54:13 +0200

    account.controller

--- src/api/account.route.ts
+++ src/api/account.route.ts
@@ -1,33 +1,10 @@
 import { Request, Response, Router } from "express";
 import { bodyValidation } from "../middlewares/validtation.middleware";
 import { LoginSchema, LoginType } from "../schema/login";
-import UserModel from "../models/user.model";
-import bcrypt from "bcrypt";
-import NotAuthorized from "../errors/NotAuthorized";
-import { sign } from "jsonwebtoken";
-import { auth } from "../middlewares/auth.middleware";
+import * as accountController from '../controllers/account.controller';
 
 const accountRouter = Router();
 
-accountRouter.post('/login', bodyValidation(LoginSchema) , async (req: Request, res: Response, next: Function) => {
-    const { username, password }: LoginType = req.body;
-
-    const user = await UserModel.findByUsername(username);
-    
-    const checkPassword = await bcrypt.compare(password, user.password);
-    
-
-    if(!checkPassword) {
-        return next(new NotAuthorized('Invalid password'));
-    }
-
-    const token = sign({id: user.id, createdAt: Date.now}, process.env.SECRET as string)
-
-
-    return res.status(200).send({
-        token
-    })
-    
-})
+accountRouter.post('/login', bodyValidation(LoginSchema) , accountController.login );
 
 export default accountRouter;
---
+++ src/controllers/account.controller.ts
@@ -0,0 +1,29 @@
+import { Request, RequestHandler, Response, Router } from "express";
+import { bodyValidation } from "../middlewares/validtation.middleware";
+import { LoginSchema, LoginType } from "../schema/login";
+import UserModel from "../models/user.model";
+import bcrypt from "bcrypt";
+import NotAuthorized from "../errors/NotAuthorized";
+import { sign } from "jsonwebtoken";
+import { auth } from "../middlewares/auth.middleware";
+
+export const login : RequestHandler = async (req, res, next) => {
+    const { username, password }: LoginType = req.body;
+
+    const user = await UserModel.findByUsername(username);
+    
+    const checkPassword = await bcrypt.compare(password, user.password);
+    
+
+    if(!checkPassword) {
+        return next(new NotAuthorized('Invalid password'));
+    }
+
+    const token = sign({id: user.id, createdAt: Date.now}, process.env.SECRET as string)
+
+
+    return res.status(200).send({
+        token
+    })
+    
+}
